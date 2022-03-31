7b4c078ff75d1bf97bf27a2fd3a7343fb9ae5069
Namito Yokota <nyokota@crimson.ua.edu>
2022-03-31 14:07:01 -0500

    added new types to retrieve from sheets

--- src/abstractions/type.ts
+++ src/abstractions/type.ts
@@ -1,8 +1,13 @@
 export enum Types {
     account = 'Account',
     book = 'Book',
+    code = 'Code',
     course = 'Course',
+    food = 'Food',
+    film = 'Film',
     job = 'Job',
+    place = 'Place',
     note = 'Note',
     website = 'Website',
+    tool = 'Tool'
 }
